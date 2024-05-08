788a6cacb80e28ea1cd212430568b09ac178a646
Dimitris Kaklamanis <dimitris.kaklamanis@safesize.com>
2024-05-08 16:16:00 +0300

    initial commit

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Plugin Name: Breakdance Custom Elements
+ * Plugin Name: Woo Product Title SEO
  * Plugin URI: https://breakdance.com/
  * Description: Boilerplate plugin to save your custom elements created with Element Studio.
  * Author: Breakdance
